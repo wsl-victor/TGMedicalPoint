5c60ec58ff65a7446786305e52053477b97c7c42
victor <apple_victor@163.com>
2016-10-28 13:26:25 +0800

    Initial Commit

---
+++ TGMedicalPoint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TGMedicalPoint
+//
+//  Created by victor on 2016/10/28.
+//  Copyright © 2016年 wsl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TGMedicalPoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TGMedicalPoint
+//
+//  Created by victor on 2016/10/28.
+//  Copyright © 2016年 wsl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
